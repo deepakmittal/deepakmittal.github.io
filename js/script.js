17a8f95853a6767dbff4a5b8109ba756e2075ae2
Deepak Mittal <deepak@vdopia.com>
2017-10-04 14:31:40 +0530

    adding rotation

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@
     }
     function initializeGlobe() {
         var earth = new WE.map('earth_div');
-        WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        WE.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
           attribution: ''
         }).addTo(earth);
       }
