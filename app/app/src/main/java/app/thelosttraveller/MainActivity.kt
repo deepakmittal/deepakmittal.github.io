89cf5d5b420cac69888522c5a9506a61f408549f
Deepak Mittal <deepakmittal@rivigo.com>
2021-07-02 00:49:02 +0530

    adding app

---
+++ app/app/src/main/java/app/thelosttraveller/MainActivity.kt
@@ -0,0 +1,25 @@
+package app.thelosttraveller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.settings.setJavaScriptEnabled(true)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://deepakmittal.github.io/")
+        webView.setPadding(0, 0, 0, 0);
+    }
+}
